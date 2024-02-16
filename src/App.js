62e33983028899d118c848e03a4f193d2b18f00e
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2024-02-16 20:48:28 +0700

    responsive sidebar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Sidebar from './Template/Side';
 import Header from './Header';
 import Dashboard from './Dashboard';
 import AccountAdmin from './AccountAdmin';
+import './Style/StyleApp.css';
 
 const App = () => {
     return (
         <Router>
             <div className="flex h-screen">
-                <div className="w-[190px]">
+                <div className="sidebar w-[190px]">
                     <Sidebar />
                 </div>
                 <div className = "flex-1 overflow-y-auto bg-[#ECEDF2] p-4" >
--- src/Template/Side.js
+++ src/Template/Side.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../Assets/logo.png';
-import { IoMenuSharp } from "react-icons/io5";
+import '../Style/StyleSidebar.css';
 import { MdDashboard } from 'react-icons/md';
 import { AiFillDatabase } from "react-icons/ai";
 import { IoMdHelpCircle } from "react-icons/io";
@@ -15,85 +15,82 @@ import { IoIosCamera } from "react-icons/io";
 const Side = () => {
     return (
         <>
-         <div className = "bg-white shadow-lg shadow-gray-400 h-screen w-[190px] overflow-auto px-[15px] fixed" >
-            <div className = "flex justify-between mt-[20px]" >
-                <img className="w-[100px]" src={Logo} alt="Deskripsi Gambar" draggable="false" />
-                <div className='bg-blue-600 p-[2px] rounded-lg hover:bg-blue-700'>
-                    <IoMenuSharp className="text-[18px] cursor-pointer text-white mt-[1px]"/>
-                </div>
+        <div className = "sidebar bg-white shadow-lg shadow-gray-400 h-screen w-[190px] overflow-auto px-[15px] fixed" id="sidebar">
+            <div className = "flex justify-center mt-[20px]" >
+                <img className="logo-company w-[100px]" src={Logo} alt="Deskripsi Gambar" draggable="false" />
             </div>
             <hr className="mt-[30px] border-t-2 border-gray-200" />
-            <div>
+            <div className="sidebar-menus">
                <ul>
                     <li className = "text-black mb-2 mt-[30px] hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <MdDashboard className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Dashboard</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <MdDashboard className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Dashboard</p>
                             </div>
                         </Link>
                     </li>
                     <li className = "text-black mb-2 hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <AiFillDatabase className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Company Data</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <AiFillDatabase className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Company Data</p>
                             </div>
                         </Link>
                     </li>
                     <li className = "text-black mb-2 hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <IoMdHelpCircle  className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Data Solutions</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <IoMdHelpCircle  className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Data Solutions</p>
                             </div>
                         </Link>
                     </li>
                     <li className = "text-black mb-2 hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <FaLightbulb  className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Data Innovation</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <FaLightbulb  className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Data Innovation</p>
                             </div>
                         </Link>
                     </li>
                     <li className = "text-black mb-2 hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <AiFillMessage className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Testimonial Data</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <AiFillMessage className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Testimonial Data</p>
                             </div>
                         </Link>
                     </li>
                     <li className = "text-black mb-2 hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <IoIosCamera className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Data Blog</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <IoIosCamera className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Data Blog</p>
                             </div>
                         </Link>
                     </li>
                     <li className = "text-black mb-2 hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <AiFillPicture className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Portfolio Data</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <AiFillPicture className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Portfolio Data</p>
                             </div>
                         </Link>
                     </li>
                     <li className = "text-black mb-2 hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <FaUsers className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Data Teams</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <FaUsers className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Data Teams</p>
                             </div>
                         </Link>
                     </li>
                     <li className = "text-black mb-2 hover:bg-[#1762EF] py-[10px] rounded-[10px] hover:text-white transition duration-75" >
                          <Link to="/">
-                            <div className="flex items-center mx-[10px]">
-                                <IoIosSettings className="mr-2 text-[18px]" />
-                                <p className="text-[13px]">Services Data</p>
+                            <div className="icon-teks flex items-center mx-[10px]">
+                                <IoIosSettings className="icon-link mr-2 text-[18px]" />
+                                <p className="menu-link text-[13px]">Services Data</p>
                             </div>
                         </Link>
                     </li>
